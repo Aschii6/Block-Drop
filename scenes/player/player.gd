19fddf6d972443e6d8199446d1cf152eb703b829
Lechea Daniel-Ioan <danutlechea2003@yahoo.com>
2025-02-26 21:38:01 +0200

    Different player controls

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,18 +2,32 @@ extends CharacterBody2D
 
 @export var player_number: int = 1
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -600.0
 
+var jump: String
+var left: String
+var right: String
+
+func _ready() -> void:
+	var numberString
+	if (player_number == 1):
+		numberString = "one"
+	else:
+		numberString = "two"
+		
+	jump = "player_" + numberString + "_jump"
+	left = "player_" + numberString + "_left"
+	right = "player_" + numberString + "_right"
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed(jump) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis(left, right)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
